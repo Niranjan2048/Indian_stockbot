497322a25955a7b1447e863b3e6a03c07e3875d1
Niranjan <niranjankhedkar23@gmail.com>
2023-09-03 10:44:01 +0530

    2nd commit

--- textbase_hackathon.py
+++ textbase_hackathon.py
@@ -21,3 +21,37 @@ def stock_info(company):
     company=company+'.NS'
     data = yf.Ticker(company).info
     return str(data)
+def calculate_SMA(company,window):
+    company=company+'.NS'
+    df = yf.Ticker(company).history(period='1y')
+    df['SMA'] = df['Close'].rolling(window=window).mean()
+    return str(df['SMA'].iloc[-1])
+
+def calculate_EMA(company,window):
+    company=company+'.NS'
+    df = yf.Ticker(company).history(period='1y')
+    df['EMA'] = df['Close'].ewm(span=window,adjust=False).mean()
+    return str(df['EMA'].iloc[-1])
+
+def calculate_MACD(company):
+    company=company+'.NS'
+    df = yf.Ticker(company).history(period='1y').Close
+    short_ema = df.ewm(span=12,adjust=False).mean()
+    long_ema = df.ewm(span=26,adjust=False).mean()
+    MACD=short_ema-long_ema 
+    signal = MACD.ewm(span=9,adjust=False).mean()
+    MACD_hist = MACD - signal
+    return f'{MACD[-1]},{signal[-1]},{MACD_hist[-1]}'
+
+
+def calculate_RSI(company):
+    company=company+'.NS'
+    data = yf.Ticker(company).history(period='1y')
+    delta = data['Close'].diff(1)
+    up=delta.clip(lower=0)
+    down=delta.clip(upper=0).abs()
+    ema_up=up.ewm(com=13,adjust=False).mean()
+    ema_down=down.ewm(com=13,adjust=False).mean()
+    rs=ema_up/ema_down
+    rsi=100-(100/(1+rs))
+    return str(rsi.iloc[-1])
